9195a5ab7fc6e2caf187106f10c645864c12ff74
Shrey Jariwala <shrey.jari@outlook.com>
2025-03-20 18:14:04 -0400

    analyser.rb home work code

--- app/controllers/learning.rb
+++ app/controllers/learning.rb
@@ -1,3 +1,11 @@
-puts "What is your first name?"
+puts "Enter your first name?"
 firstname = gets.chomp
 puts "Thank you, you said your first name is #{firstname}."
+
+puts "Enter your last name?"
+lastname = gets.chomp
+puts "Thank you, you said your last name is #{lastname}"
+
+puts "Your full name is #{firstname} #{lastname}"
+puts "Your full name reversed is #{firstname.reverse} #{lastname.reverse}"
+puts "Your full name has #{firstname.length + lastname.length} characters in it."
